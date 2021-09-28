602b2b35bd62bac5914b8039a0bc6a4a18ed88a1
Esoteric918 <3411@holbertonschool.com>
2021-09-28 13:41:02 -0700

    pep this

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -3,12 +3,14 @@ import random
 number = random.randint(-10000, 10000)
 
 modnum = number % 10 if number > 0 else number % -10
+dum = "less than 6 and not 0"
+dummer = "greater than 5"
 
 if modnum > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, modnum))
+    print("Last digit of {} is {} and is {}".format(number, modnum, dummer))
 
 elif modnum == 0:
     print("Last digit of {} is {} and is 0".format(number, modnum))
 
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, modnum))
+    print("Last digit of {} is {} and is {}".format(number, modnum, dum))

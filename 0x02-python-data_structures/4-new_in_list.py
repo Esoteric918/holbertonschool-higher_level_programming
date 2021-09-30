812358c40824ca2923de5ba7bb08b543d8ec9c9a
Esoteric918 <3411@holbertonschool.com>
2021-09-30 11:52:11 -0700

    copy list and add new ele

---
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    z = len(my_list)
+    new = my_list[:]
+    if idx < 0 and idx <= z:
+        return my_list
+    new[idx] = element
+    return new

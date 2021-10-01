0c22174a9aaff4af1dd136b8a7c96c2f0844413c
Esoteric918 <3411@holbertonschool.com>
2021-09-30 20:55:30 -0700

    fix var call

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list
-    newList = []
-    for i in my_list:
-        newList.append(False if x % 2 else True)
-    return newList
+    if my_list:
+        newList = []
+        for i in my_list:
+            newList.append(False if i % 2 else True)
+        return newList

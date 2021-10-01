d3d55bdf77e16701854a5cadaafd70f3b232a4cc
Esoteric918 <3411@holbertonschool.com>
2021-09-30 18:53:08 -0700

    add tuple

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -6,11 +6,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
     len_a = len(tupa)
     len_b = len(tupb)
     while len_a < 2:
-       tupa.append(zero)
-       len_a += 1
+        tupa.append(zero)
+        len_a += 1
     while len_b < 2:
-       tupb.append(zero)
-       len_b += 1
+        tupb.append(zero)
+        len_b += 1
     x = tupa[0] + tupb[0]
     z = tupa[1] + tupb[1]
     return x, z

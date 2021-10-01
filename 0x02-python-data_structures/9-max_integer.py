989e2cb570e2794359a65f7cc4bee72d7f3ea786
Esoteric918 <3411@holbertonschool.com>
2021-09-30 20:05:34 -0700

    tofer style

--- 0x02-python-data_structures/9-max_integer.py
+++ 0x02-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == 0:
+    if not my_list:
         return None
     largest = 0
-    for x in range(0, len(my_list)):
-        if(my_list[x] > largest):
-            largest = my_list[x]
+    for x in my_list:
+        if(x > largest):
+            largest = x
     return largest

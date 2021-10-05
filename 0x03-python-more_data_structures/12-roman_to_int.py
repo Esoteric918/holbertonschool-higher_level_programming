8cf48fcb1e8e4538f97d5121d87fe5ce0852731c
Esoteric918 <3411@holbertonschool.com>
2021-10-05 00:22:58 -0500

    to the wire

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    romValue = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    romValue = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                'C': 100, 'D': 500, 'M': 1000}
     intVal = 0
     rome = roman_string
     if type(roman_string) != str or roman_string is None:

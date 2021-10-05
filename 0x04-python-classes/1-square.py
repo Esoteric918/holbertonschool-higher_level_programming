3010e23ffd21fb2e94293a7d05df5ea650382f5a
Esoteric918 <3411@holbertonschool.com>
2021-10-05 11:23:30 -0500

    that defines a square by: (based on 0-square.py)

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" Defines Square by size"""
+__size = None
+
+class Square:
+    """ Defines size with varification """
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")

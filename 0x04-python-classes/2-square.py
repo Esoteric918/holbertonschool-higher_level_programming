bb3ad484b035174c658b13661fb5cf87360dc128
Esoteric918 <3411@holbertonschool.com>
2021-10-05 12:27:34 -0500

    Defines Square by area

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" Defines Square by size"""
+
+
+class Square:
+    """ Defines size with varification """
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    """Defines Square of area"""
+
+    def area(self, size=0):
+        sr_area = self.__size ** 2
+        return sr_area

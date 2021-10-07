0dc6c2d6db9f5bc4b635386732b3a908df48fed0
Esoteric918 <3411@holbertonschool.com>
2021-10-07 11:15:23 -0500

    print alphbet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print(*["%c" % a for a in range(ord('a'),
-      ord('z')+1) if a not in (ord('q'), ord('e'))], sep='', end='')
+for i in range(97, 123):
+    print(chr(i), end="", )

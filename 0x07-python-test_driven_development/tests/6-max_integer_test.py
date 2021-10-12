8065e28a0a53bd04c2efb52594fe296cafdb02a4
Esoteric918 <3411@holbertonschool.com>
2021-10-12 01:29:26 -0500

    make checker happy

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,10 +1,7 @@
 """Unittest for max_integer([..])
 """
-
-
 import unittest
 max_integer = __import__('6-max_integer').max_integer
-
 
 class TestMaxInteger(unittest.TestCase):
     """Tests for max_integer"""

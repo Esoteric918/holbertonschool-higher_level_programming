fbfa221ea5fea5cc6d0e56a719c14798bd69d612
Esoteric918 <3411@holbertonschool.com>
2021-10-12 01:12:47 -0500

    if __name__ == '__main__':

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python3
 """Unittest for max_integer([..])
 """
 
 
 import unittest
 max_integer = __import__('6-max_integer').max_integer
+
+
 class TestMaxInteger(unittest.TestCase)
-    """
-    Tests for max_integer()
-    """"
+    """Tests for max_integer"""
+
     def max_integer(list=[]):
         self.assertEqual(max_integer([1,2,3,4]), 4)
         self.assertEqual(max_integer([-1,-2,-3,-4]), -1)

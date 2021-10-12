a7032acf4299f24b226167803eabcc13dc409ab4
Esoteric918 <3411@holbertonschool.com>
2021-10-12 02:27:54 -0500

    fix if statment placement

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,13 +11,13 @@ def add_integer(a, b=98):
     Returns:
         int(a + b)
     '''
-
+    if type(a) == float or type(b) == float:
+        a = int(a)
+        b = int(b)
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
         raise TypeError("b must be an integer")
-    if type(a) == float or type(b) == float:
-        a = int(a)
-        b = int(b)
+
     else:
         return a + b

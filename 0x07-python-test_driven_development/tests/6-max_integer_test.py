0cf54fe44a83c2510e04d349d5f6a0178e349100
Esoteric918 <3411@holbertonschool.com>
2021-10-12 01:09:28 -0500

    if __name__ == '__main__':

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -16,3 +16,6 @@ class TestMaxInteger(unittest.TestCase)
         self.assertEqual(max_integer(['w','z', 'a', 'd']), 'z')
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([1]), 1
+
+    if __name__ == '__main__':
+        unittest.main()

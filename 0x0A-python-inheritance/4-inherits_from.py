509e69e8b0f8311a681d804bac96683a78a1b58e
Esoteric918 <3411@holbertonschool.com>
2021-10-13 12:03:55 -0500

    Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""inherits_form
+"""
+def inherits_from(obj, a_class):
+    """ Returns TRUE if form the same subclass
+    """
+    return (issubclass(type(obj), a_class) and type(obj) is not a_class)

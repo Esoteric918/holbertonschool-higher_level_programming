18bc1c3adae4d2cf207e32a93449188ab1e4d89f
Esoteric918 <3411@holbertonschool.com>
2021-10-13 18:53:40 -0500

    fir comments and raise syntax

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,10 +4,13 @@
 
 
 class BaseGeometry:
-    """ raises Exception
+    """Contains functions area(), integer_validator()
     """
 
+
     def area(self):
+        """ raises Exception
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
@@ -15,6 +18,6 @@ class BaseGeometry:
         """
 
         if type(value) is not int:
-            raise TypeError(name, "must be an integer")
+            raise TypeError(name + "must be an integer")
         if value <= 0:
-            raise ValueError(name, "must be greater than 0")
+            raise ValueError(name + "must be greater than 0")

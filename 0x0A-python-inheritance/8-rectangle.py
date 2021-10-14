4537844a474e3aa42b2474d9a5e7acb50d0e0949
Esoteric918 <3411@holbertonschool.com>
2021-10-13 20:17:38 -0500

    fix syntax

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle(BaseGeometry):
     """Defines the Rectangle class"""
 
     def __init__(self, width, height):
-        super().integer_validator("width", height)
-        super().integer_validator("height", width)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height

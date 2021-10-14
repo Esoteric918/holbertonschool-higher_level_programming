0c69805c0197637367cfeab964c9ae02af8a5341
Esoteric918 <3411@holbertonschool.com>
2021-10-13 19:30:47 -0500

    fix public syntax

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -9,18 +9,18 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
-        super().integer_validator(width, height)
-        super().integer_validator(height, width)
-        self.width = width
-        self.height = height
+        super().integer_validator("width", height)
+        super().integer_validator("height", width)
+        self.__width = width
+        self.__height = height
 
     def area(self):
         """Determins area
         """
-        area = self.width * self.height
+        area = self.__width * self.__height
         return area
 
     def __str__(self):
         """Magic Trick !
         """
-        return "[Rectangle] {}/{}".format(self.width, self.height)
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)

3b0cbbfa59d6f6592dd5efea63503ef1aad08a00
Esoteric918 <3411@holbertonschool.com>
2021-10-13 19:12:55 -0500

    fix pep8 issue

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -9,7 +9,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
-        super().integer_validator(width, height)
-        super().integer_validator(height, width)
-        self.width = width
-        self.height = height
+        super().integer_validator("width", height)
+        super().integer_validator("height", width)
+        self.__width = width
+        self.__height = height

5daff26131df2052bbf35785c0dab965c4588ef3
Esoteric918 <3411@holbertonschool.com>
2021-10-13 19:20:05 -0500

    fix syntax

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -14,11 +14,11 @@ class Square(Rectangle):
     def __init__(self, size):
         super().integer_validator(size, size)
         super().__init__(size, size)
-        self.size = size
+        self.__size = size
 
     def area(self):
 
         return self.size * self.size
 
     def __str__(self):
-        return "[Square] {}/{}".format(self.size, self.size)
+        return "[Square] {}/{}".format(self.__size, self.__size)

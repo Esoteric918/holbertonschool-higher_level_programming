e0eb354d93a645eb832e859a6c6009828f191f41
Esoteric918 <3411@holbertonschool.com>
2021-10-15 10:03:03 -0500

    pep happy

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Appends file text
+"""
+
+def append_write(filename="", text=""):
+    with open(filename, 'a', encoding='utf=8') as appendFile:
+        appendFile.write(text)
+        return len(text)

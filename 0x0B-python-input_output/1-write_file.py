a8149c460c95ef83550e2ebcf12f08a553186987
Esoteric918 <3411@holbertonschool.com>
2021-10-15 09:53:24 -0500

    fix for pep8

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" Writes to a file"""
+
+
+def write_file(filename="", text=""):
+    """ Writes to a file makes file if not there
+    rewrites if text is there"""
+
+    with open(filename, 'w', encoding='utf-8') as writeFile:
+        writeFile.write(text)
+        return len(text)

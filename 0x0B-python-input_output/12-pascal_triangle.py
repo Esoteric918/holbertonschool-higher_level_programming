ffdc57f191fdedd3186ee90df9951f907490c383
Esoteric918 <3411@holbertonschool.com>
2021-10-15 21:41:00 -0500

    almost got it

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,16 +4,15 @@
 
 
 def pascal_triangle(n):
-    arr = [[0 for x in range(n)]
-              for y in range(n)]
+    arr = [[0 for x in range(n)] for y in range(n)]
 
-    for line in range (0, n):
-        for i in range (0, line + 1):
+    for line in range(0, n):
+        for i in range(0, line + 1):
             if(i is 0 or i is line):
                 arr[line][i] = 1
-                print(arr[line][i], end = " ")
+                print(arr[line][i], end=" ")
             else:
-                arr[line][i] = (arr[line - 1][i - 1] +
-                arr[line - 1][i])
-                print(arr[line][i], end = " ")
+                arr[line][i] = (arr[line - 1][i - 1] + arr[line - 1][i])
+
+                print(arr[line][i], end=" ")
         print("\n", end="")

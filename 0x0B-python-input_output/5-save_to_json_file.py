6806280247bcbf57879fc406245e6a09eeb18689
Esoteric918 <3411@holbertonschool.com>
2021-10-15 20:19:07 -0500

    fix whitespace error

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,8 +7,8 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Writes an object to a text file, using JSON
+    """Writes an object to a text file, using JSON rep
     """
 
-    with open(filename) as newFile:
+    with open(filename, 'w') as newFile:
         newFile.write(json.dumps(my_obj))

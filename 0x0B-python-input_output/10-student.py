826adc8b802fc61df9160b9e98cac8e7c86ed1cb
Esoteric918 <3411@holbertonschool.com>
2021-10-15 20:34:13 -0500

    fix indentation error

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Student
+"""
+
+
+class Student:
+    """Contains student data
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary of Student
+        """
+
+        attrString = {}
+
+        if attrs is not None:
+            for key in attrs:
+                if key in self.__dict__:
+                    attrString[key] = self.__dict__[key]
+            return attrString
+        else:
+            return vars(self)

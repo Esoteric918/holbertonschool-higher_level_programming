05c8cdc2aa660f4f4fdba07dcf5edc02cf15a557
Esoteric918 <3411@holbertonschool.com>
2021-10-21 17:22:08 -0500

    fix pep8 issues

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,11 +5,11 @@
 import json
 
 
-
 class Base:
     """Base of the other shapes
     """
-    __nb_objects = 0 # private class attribute
+
+    __nb_objects = 0  # private class attribute
 
     def __init__(self, id=None):
         if id is not None:
@@ -36,7 +36,7 @@ class Base:
         """Returns JSON string representation
         """
         newLd = []
-        if list_dictionaries == None or list_dictionaries == "":
+        if list_dictionaries is None or list_dictionaries == "":
             return newLd
         else:
             newLd = json.dumps(list_dictionaries)
@@ -46,18 +46,18 @@ class Base:
     def save_to_file(cls, list_objs):
         """Writes to file with JSON string
         """
-        with open (cls.__name__ +".json", 'w') as write_file:
-            if list_objs == None:
+        with open(cls.__name__ + ".json", 'w') as write_file:
+            if list_objs is None:
                 write_file.write('[]')
             else:
                 write_file.write(cls.to_json_string(
-                [item.to_dictionary() for item in list_objs]))
+                    [item.to_dictionary() for item in list_objs]))
 
     @staticmethod
     def from_json_string(json_string):
         """Returns JSON strings in list
         """
-        if json_string == None:
+        if json_string is None:
             return ([""])
         else:
             return json.loads(json_string)
@@ -76,17 +76,13 @@ class Base:
     @classmethod
     def load_from_file(cls):
         lists = []
-        with open(cls.__name__ +'.json', 'r') as readFile:
-             text = readFile.read()
+        with open(cls.__name__ + '.json', 'r') as readFile:
+            text = readFile.read()
 
         text = cls.from_json_string(text)
-        print('text', text)
         for i in text:
-            print('i',i)
-
             if type(i) == dict:
                 lists.append(cls.create(**i))
             else:
                 lists.append(i)
             return lists
-
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,8 +4,8 @@ Square
 """
 from models.rectangle import Rectangle
 
-class Square(Rectangle):
 
+class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
 

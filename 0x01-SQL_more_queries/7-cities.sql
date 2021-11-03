ca866a41844520b4e715c4ceeb6f0bbd16e1b780
Esoteric918 <3411@holbertonschool.com>
2021-11-03 12:12:47 -0700

    add comment

--- 0x01-SQL_more_queries/7-cities.sql
+++ 0x01-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE if not EXISTS hbtn_0d_usa;
 CREATE TABLE if not EXISTS hbtn_0d_usa.cities (
     id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(256),
+    name VARCHAR(256) NOT NULL,
     PRIMARY KEY(id),
     state_id INT NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));

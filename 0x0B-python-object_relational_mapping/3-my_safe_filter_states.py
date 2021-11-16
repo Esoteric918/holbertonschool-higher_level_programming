5e989846d33d0646e02dd124330479634be48d75
Esoteric918 <3411@holbertonschool.com>
2021-11-16 13:43:17 -0600

    correction to execute

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,7 +3,7 @@
 import sys
 import MySQLdb
 
-def main(argv):
+def safe_states():
      """Takes arguments argv to list from database
     Only lists with states that matches name argument
     Arguments:
@@ -12,20 +12,18 @@ def main(argv):
         argv[3]: database name
         argv[4]: state name
     """
-    if len(sys.argv) == 5:
-        conn = MySQLdb.connect(host="localhost",
-                                port=3306,
-                                user=sys.argv[1],
-                                passwd=sys.argv[2],
-                                db=sys.argv[3])
+if len(sys.argv) == 5:
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                       passwd=sys.argv[2], db=sys.argv[3])
 
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM states WHERE name = %s",(sys.argv[4],))
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s\
+                ORDER BY id ASC",(sys.argv[4],))
 
-        for row in cur.fetchall():
-            print(row)
-        cur.close()
-        conn.close()
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    conn.close()
 
 if __name__ == "__main__":
-     main()
+    safe_states()

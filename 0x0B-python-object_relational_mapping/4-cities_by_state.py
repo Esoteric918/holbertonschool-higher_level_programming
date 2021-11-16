28e556396325e3f99ae5f004fc11c2cfdfdb9093
Esoteric918 <3411@holbertonschool.com>
2021-11-16 14:31:11 -0600

    fix pycodestyle

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -12,11 +12,8 @@ def list_cities():
         argv[2]: mysql password
         argv[3]: database name
     """
-    conn = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
 
     cur = conn.cursor()
 
@@ -28,6 +25,7 @@ def list_cities():
         print(row)
     cur.close()
     conn.close()
+
 
 if __name__ == "__main__":
     list_cities()

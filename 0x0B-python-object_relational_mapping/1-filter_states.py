c761de525c6f55a441428c9fc94532fc8b5a2ada
Esoteric918 <3411@holbertonschool.com>
2021-11-16 12:57:45 -0600

    fix pycodestyle

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -14,11 +14,8 @@ def state_N(argv):
         argv[2]: mysql password
         argv[3]: database name
     """
-    conn = MySQLdb.connect(host="localhost",
-                            port=3306,
-                            user=sys.argv[1],
-                            passwd=sys.argv[2],
-                            db=sys.argv[3])
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
 
     cur = conn.cursor()
 

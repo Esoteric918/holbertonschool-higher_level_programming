2f9ac3c38aad7b6280641294a50d83f8c90a66bb
Esoteric918 <3411@holbertonschool.com>
2021-11-16 12:54:13 -0600

    fixed pycodestyle

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -14,7 +14,7 @@ def get_states():
         argv[3]: database name
     """
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv, db=sys.argv[3])
+                           passwd=sys.argv[2], db=sys.argv[3])
 
     cur = conn.cursor()
 

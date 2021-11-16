09ea40431cf73094ca5ef2d5e03b8c9600c3a18b
Esoteric918 <3411@holbertonschool.com>
2021-11-16 14:24:45 -0600

    fix pycodestyle

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE BINARY name '{}'\
-                ORDER BY state.id ASC".format(sys.agrv[4]))
+                ORDER BY state.id ASC".format(sys.argv[4]))
 
     for row in cur.fetchall():
         print(row)

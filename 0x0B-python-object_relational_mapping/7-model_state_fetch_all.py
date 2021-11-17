a13eff175456d567e200ac5366a4bad417d774e1
Esoteric918 <3411@holbertonschool.com>
2021-11-17 13:51:58 -0600

    forgot pycode fix

--- 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""lists all State objects from the database"""
 import sys
 import sqlalchemy
 from sqlalchemy import (create_engine)
@@ -7,12 +7,14 @@ from sqlalchemy.engine.base import Engine
 from model_state import Base, State
 from sqlalchemy.orm import query_expression, session, sessionmaker
 
+
 if __name__ == "__main__":
 
     Session = sessionmaker(Engine)
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                        .format(sys.argv[1], sys.argv[2],
-                        sys.argv[3]), pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2],
+                           sys.argv[3]), pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
     Base.metadata.create_all(engine)
 

a33743326033261bff9894f1a34fe363f56ac9be
Esoteric918 <3411@holbertonschool.com>
2021-11-17 08:22:27 -0600

    things change

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -4,30 +4,18 @@ import MySQLdb
 import sys
 
 
-def list_cities():
-    """Takes arguments argv to list from database
-    Only lists with states that matches name argument
-    Arguments:
-        argv[1]: mysql username
-        argv[2]: mysql password
-        argv[3]: database name
-    """
-    if len(sys.argv) == 5:
+if __name__ == "__main__":
         conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                             passwd=sys.argv[2], db=sys.argv[3])
 
-    cur = conn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name \
-                FROM cities INNER JOIN states ON cities.state_id = states.id\
-                ORDER BY cities.id ACS")
+        cur = conn.cursor()
+        cur.execute("SELECT cities.id, cities.name, states.name \
+                    FROM cities INNER JOIN states ON cities.state_id = states.id\
+                    ORDER BY cities.id ACS")
 
-    thing = cur.fetchall()
-    for row in thing:
-        print(row)
+        thing = cur.fetchall()
+        for row in thing:
+            print(row)
 
-    cur.close()
-    conn.close()
-
-
-if __name__ == "__main__":
-    list_cities()
+        cur.close()
+        conn.close()

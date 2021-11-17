ffd07a591ef2a51a9fe679dc610a689d093730a0
Esoteric918 <3411@holbertonschool.com>
2021-11-17 07:58:45 -0600

    pycodestyle fix

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -14,15 +14,17 @@ def list_cities():
     """
     if len(sys.argv) == 5:
         conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3])
+                               passwd=sys.argv[2], db=sys.argv[3])
 
     cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+                FROM cities INNER JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id ACS")
 
-    cur.execute("SELECT cities.id, cities.name, states.name FROM\
-    cities INNER JOIN states ON cities.state_id = states.id;")
-
-    for row in cur.fetchall():
+    thing = cur.fetchall()
+    for row in thing:
         print(row)
+
     cur.close()
     conn.close()
 

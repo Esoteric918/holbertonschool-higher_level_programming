4ec523612355892d9a3d03e8b8cbe007c0e87aba
Esoteric918 <3411@holbertonschool.com>
2021-11-17 14:04:19 -0600

    made executable

--- 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""lists all State objects from the database"""
+"""lists all State objects from the database SQLalchemy method"""
 import sys
 import sqlalchemy
 from sqlalchemy import (create_engine)

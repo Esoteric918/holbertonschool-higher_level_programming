b7e599cb9a387170dd6509d3fd02f5eec2ae5091
Esoteric918 <3411@holbertonschool.com>
2021-11-18 12:08:08 -0600

    class for City

--- 0x0B-python-object_relational_mapping/model_city.py
+++ 0x0B-python-object_relational_mapping/model_city.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""module for state class"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """class for States"""
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)

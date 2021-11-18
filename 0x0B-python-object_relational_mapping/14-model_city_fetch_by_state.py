a21e5e52eb338bd82b5fa14c06332c0ff810e79c
Esoteric918 <3411@holbertonschool.com>
2021-11-18 12:08:44 -0600

    list state and city

--- 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""lists first State object from the database SQLalchemy method"""
+from sys import argv
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
+from model_city import Base, City
+from model_state import Base, State
+from sqlalchemy.orm import query_expression, session, sessionmaker
+
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    sess = Session()
+
+    s = Session()
+    for city, State in s.query(City, State).order_by(City.id):
+        print("{}: ({}) {}".format(State.name, city.id, city.name))
+    s.close

dc9e013a1ab0e835ff3c292934099cad821a6a48
Esoteric918 <3411@holbertonschool.com>
2021-11-18 10:29:50 -0600

    update id 2

--- 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""lists first State object from the database SQLalchemy method"""
+from sys import argv
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
+from model_state import Base, State
+from sqlalchemy.orm import query_expression, session, sessionmaker
+
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    sess = Session()
+
+    changed = sess.query(State).get(2)
+    changed.name = "New Mexico"
+
+    sess.commit()

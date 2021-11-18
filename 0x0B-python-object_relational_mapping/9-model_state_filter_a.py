ce45d5e4ace09f75ea2e31f84cc47908f4eada3c
Esoteric918 <3411@holbertonschool.com>
2021-11-17 21:30:47 -0600

    test commit

--- 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,22 @@
-#!/usr/bin/python3
+"""lists first State object from the database SQLalchemy method"""
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
+from model_state import Base, State
+from sqlalchemy.orm import query_expression, session, sessionmaker
+
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    s = session()
+    for state in s.query(State).order_by(State.id)\
+        .filter(State.name.like('%a%')):
+        print("{}: {}".format(state.id, state.name))
+    s.close

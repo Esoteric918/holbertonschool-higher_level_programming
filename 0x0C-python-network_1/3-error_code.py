178d3d68b150aaf17bb982fed71b627b5784666c
Esoteric918 <3411@holbertonschool.com>
2021-12-01 13:23:34 -0600

    for the checks

---
+++ 0x0C-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Error codes"""
+from urllib import error, request, parse
+from sys import argv
+import urllib
+
+
+def errorCode():
+    try:
+        with request.urlopen(argv[1]) as response:
+            html = response.read()
+            print(html.decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code:".format(e.code))
+
+if __name__ == "__main__":
+    errorCode()

53f2fd09ebea26643fa6fc60477dfe8042910df3
Esoteric918 <3411@holbertonschool.com>
2021-12-01 14:22:34 -0600

    request

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """whats my status? #0"""
-import urllib.request
+import requests
 
 
 if __name__ == "__main__":
 
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as resp:
-        html = resp.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
+    x = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(x.text)))
+    print("\t- content: {}".format(x.text))

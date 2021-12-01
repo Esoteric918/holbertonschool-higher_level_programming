c5a859629eb152c678e581ffcd7de4ef38670e2b
Esoteric918 <3411@holbertonschool.com>
2021-12-01 13:45:12 -0600

    corrrction to syntx

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """Error codes"""
 import urllib.request
+import urllib.error
 import sys
 
 
 def errorCode():
     """Error code check"""
     url = sys.argv[1]
-    req =urllib.request.Request(url)
+    req = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(req) as resp:
-            print(resp.read().decode("utf-8"))
+            html = resp.read()
+            print(html.decode("utf-8"))
     except urllib.error.HTTPError as e:
         print("Error code:".format(e.code))
+
 
 if __name__ == "__main__":
     errorCode()

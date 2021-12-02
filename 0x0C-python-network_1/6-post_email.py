66afe904b2afc64e40ac308f2f8e5027e3e8a11e
Esoteric918 <3411@holbertonschool.com>
2021-12-01 18:58:49 -0600

    pep8

--- 0x0C-python-network_1/6-post_email.py
+++ 0x0C-python-network_1/6-post_email.py
@@ -3,6 +3,7 @@
 import requests
 import sys
 
+
 if __name__ == "__main__":
 
-    x = requests.post(sys.argv[1], data ={"email" == sys.argv[2]})
+    x = requests.post(sys.argv[1], data={"email" == sys.argv[2]})

0ad9aed76c321768a4f5b3aeb825c9165598b339
Esoteric918 <3411@holbertonschool.com>
2021-12-01 21:59:45 -0600

    error status

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""check error status"""
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    result = requests.get(sys.argv[1])
+    try:
+        if result.status_code > 400:
+            print("Error code: {}".format(result.status_code))
+        else:
+            print(result.content.decode("utf-8"))
+    except KeyError:
+        pass

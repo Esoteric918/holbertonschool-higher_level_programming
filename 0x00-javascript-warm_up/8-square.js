fbf1dad8491b0855a572d7939ca81dc48bf1dcdb
Esoteric918 <3411@holbertonschool.com>
2022-01-06 15:06:17 -0600

    task 8

--- 0x00-javascript-warm_up/8-square.js
+++ 0x00-javascript-warm_up/8-square.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+if (num) {
+  for (let x = 0; x < num; ++x) {
+    console.log('X'.repeat(num));
+  }
+} else {
+  console.log('Missing size');
+}

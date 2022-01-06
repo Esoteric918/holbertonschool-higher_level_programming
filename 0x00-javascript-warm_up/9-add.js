b04b120a9160303a998ae19869742528f3126774
Esoteric918 <3411@holbertonschool.com>
2022-01-06 15:38:09 -0600

    task 9

--- 0x00-javascript-warm_up/9-add.js
+++ 0x00-javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
 function add (a, b) {
   return a + b;
 }
-const a = parseInt(process.agrv[2]);
-const b = parseInt(process.agrv[3]);
-
 console.log(add(a, b));

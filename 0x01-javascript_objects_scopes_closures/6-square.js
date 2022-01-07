a834e44437027fe23c0abbe77d3e3b7c8c8635e4
Esoteric918 <3411@holbertonschool.com>
2022-01-07 13:59:26 -0600

    happy plate'

--- 0x01-javascript_objects_scopes_closures/6-square.js
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 module.exports = class Square extends require('./5-square') {
-  charPrint(c){
+  charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
     for (let x = 0; x < this.height; ++x) {
-      console.log(.repeat(this.width));
+      console.log(c.repeat(this.width));
     }
   }
 };

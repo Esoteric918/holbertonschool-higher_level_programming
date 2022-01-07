f7f67cd7cdc400ad4a92e706fb805e305b678275
Esoteric918 <3411@holbertonschool.com>
2022-01-06 18:54:43 -0600

    task 10 get factorail

--- 0x00-javascript-warm_up/10-factorial.js
+++ 0x00-javascript-warm_up/10-factorial.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+function factorail (num) {
+  if (isNaN(num) || num <= 1) {
+    return 1;
+  } else {
+    return num * factorail(num - 1);
+  }
+}
+
+console.log(factorail(num));

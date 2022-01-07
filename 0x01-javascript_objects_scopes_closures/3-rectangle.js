83320f04ad4795929d29fe516652e4d0cab766fa
Esoteric918 <3411@holbertonschool.com>
2022-01-07 12:56:17 -0600

    semistandard fix

--- 0x01-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
 
-module.exports =class Rectangle {
-  constructor(w, h) {
+module.exports = class Rectangle {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
-    this.width = w;
-    this.height = h;
+      this.width = w;
+      this.height = h;
     }
   }
+
   print () {
     for (let x = 0; x < this.height; ++x) {
-      console.log('X'.repeat(this.width))
+      console.log('X'.repeat(this.width));
     }
   }
 };

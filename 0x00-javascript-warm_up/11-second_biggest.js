acb64ad878e2e59c4df257930dae4351e07ef587
Esoteric918 <3411@holbertonschool.com>
2022-01-06 19:22:38 -0600

    2nd biggest

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const arr = process.argv.slice(2);
+  const newArr = arr.sort((a, b) => a - b);
+  if (Math.sign(newArr[1]) === -1) {
+    console.log(parseInt(newArr[1], 10));
+  } else {
+    newArr.reverse();
+    console.log(parseInt(newArr[1], 10));
+  }
+}

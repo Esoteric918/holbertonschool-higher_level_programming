5fcb8407809773ff7d81a4d5adfc1f823fa37130
Esoteric918 <3411@holbertonschool.com>
2022-01-07 13:51:14 -0600

    task 6

---
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+module.exports = class Square extends require('./5-square') {
+  charPrint(c){
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let x = 0; x < this.height; ++x) {
+      console.log(.repeat(this.width));
+    }
+  }
+};

c6f9ae4bfc26860847b9d6c69005480fb923d5c5
Esoteric918 <3411@holbertonschool.com>
2022-01-11 14:00:53 -0600

    task 4

---
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+const starWars =  process.argv[2];
+request(starWars, function (_err, res, body) {
+  let count = 0;
+  let data = JSON.parse(body);
+  for (let i = 0; i < data.length; ++i) {
+    for (let x = 0; x < data[i].characters.length; ++x){
+    if (data[i].characters[n] === '18') {
+      }
+      count++;
+    }
+  }
+  console.log(count);
+});

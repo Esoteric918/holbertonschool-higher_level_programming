99a14724dd0e3169f2061b1e1ea443d654b2e96a
Esoteric918 <3411@holbertonschool.com>
2022-01-11 12:39:35 -0600

    task 3

---
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const request = require('request');
+const starWars = 'https://swapi-api.hbtn.io/api/films/' + (process.argv[2]);
+
+request(starWars, function (_err, _res, body) {
+  const thing = JSON.parse(body);
+  console.log(thing.title);
+});

fe4ec39b5caa952c2c69c908ad4f8c6b8ded2824
Esoteric918 <3411@holbertonschool.com>
2022-01-11 12:24:26 -0600

    semi

--- 0x02-javascript-web_scraping/1-writeme.js
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let fs = require('fs')
-let data = process.argv[3]
-fs.writeFile(process.argv[2], data, 'utf-8',  (err) => {
-  if (err){
+const fs = require('fs');
+const data = process.argv[3];
+fs.writeFile(process.argv[2], data, 'utf-8', (err) => {
+  if (err) {
     console.log(err);
   }
 });
